2802431f049705f091943abc00200326044d6326
rajeeve12 <rajeevevishen21@gmal.com>
2024-09-22 15:43:57 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,7 @@
+//Task 1
+const inventory = [{name: `Burger`, price: 15, quantity: 10,},
+    { name: 'Hot Dog', price: 12, quantity: 5, },
+    { name: 'Soda', price: 5, quantity: 7,},
+    { name: 'Fries', price: 6, quantity: 15,},
+     ]
+  
